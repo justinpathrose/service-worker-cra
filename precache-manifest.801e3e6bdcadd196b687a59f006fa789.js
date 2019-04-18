85060f079000da2be2a102870a31e1aa65ce99e8
Justin Pathrose Vareed <justin.pathrosevareed@morrisonsplc.co.uk>
2019-04-18 10:19:31 +0100

    Updates

---
+++ precache-manifest.801e3e6bdcadd196b687a59f006fa789.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a56be5037f403aaa1d31",
+    "url": "/github.io/service-worker-cra/static/js/runtime~main.07d9be66.js"
+  },
+  {
+    "revision": "207f5c2c4117aad47b02",
+    "url": "/github.io/service-worker-cra/static/js/main.ddf0d3f2.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/github.io/service-worker-cra/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "207f5c2c4117aad47b02",
+    "url": "/github.io/service-worker-cra/static/css/main.90d16be2.chunk.css"
+  },
+  {
+    "revision": "af8582340b09b51330b231dbf268278f",
+    "url": "/github.io/service-worker-cra/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.64bdc05fc20db718d99c3b0551425b02.js"
+  "/github.io/service-worker-cra/precache-manifest.801e3e6bdcadd196b687a59f006fa789.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/github.io/service-worker-cra/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });

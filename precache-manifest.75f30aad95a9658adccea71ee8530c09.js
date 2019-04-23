8d09695c7ae2fb6173d2489fbc626281bdeccbaa
Justin Pathrose Vareed <justin.pathrosevareed@morrisonsplc.co.uk>
2019-04-23 12:01:52 +0100

    Updates

--- precache-manifest.75f30aad95a9658adccea71ee8530c09.js
+++ precache-manifest.75f30aad95a9658adccea71ee8530c09.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/service-worker-cra/static/js/runtime~main.f550a33c.js"
   },
   {
-    "revision": "e7308d690d51164337d8",
-    "url": "/service-worker-cra/static/js/main.463b9fdb.chunk.js"
+    "revision": "389b8c4239a914d99c82",
+    "url": "/service-worker-cra/static/js/main.83532f38.chunk.js"
   },
   {
     "revision": "681b51057d74cfa8975d",
     "url": "/service-worker-cra/static/js/2.65aa1cca.chunk.js"
   },
   {
-    "revision": "e7308d690d51164337d8",
+    "revision": "389b8c4239a914d99c82",
     "url": "/service-worker-cra/static/css/main.f0c4ce84.chunk.css"
   },
   {
-    "revision": "c52b07a62da64f511451aa04d3bf30e2",
+    "revision": "b45203c805118f44d054b68a857b450c",
     "url": "/service-worker-cra/index.html"
   }
 ];
